9708a7506dfba6aec5b60cda9fbcf3ef7be10395
Rick Hallett <rickhallett@live.co.uk>
2021-02-04 11:18:58 +0000

    TODO: res properties without sails log interface

--- server/api/controllers/EmailController.js
+++ server/api/controllers/EmailController.js
@@ -5,6 +5,7 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+const fs = require('fs');
 const emailService = require('../services/EmailService');
 
 const defaultConfigs = {
@@ -22,8 +23,37 @@ module.exports = {
       console.log('res', result);
       console.log('typeof res', typeof result);
 
+      // TODO: find a way of grabbing only the reponse properties we want for more intelligent response processing
+      // res.send(res) automatically
+      // const util = require('util');
+      // console.log(util.inspect(res, {depth: null}));
+      // fs.writeFileSync('output.txt', util.inspect(res, {depth: null}), 'utf-8');
+
+      // Object.keys(res).map(k => {
+      //   if (res.hasOwnProperty(k)) {
+      //     console.log(k)
+      //   }
+      // });
+
+      console.log(res);
+
+      // const payload = {
+      //   accepted: res.accepted,
+      //   rejected: res.rejected,
+      //   envelopeTime: res.envelopeTime,
+      //   messageTime: res.messageTime,
+      //   messageSize: res.messageSize,
+      //   response: res.response,
+      //   envelope: res.envelope,
+      //   messageId: res.messageId
+      // };
+
       res.status(200);
-      res.send('Mail sent');
+
+      
+
+      res.send(payload);
+
     })
     .catch(err => {
       sails.log.error('msg', err.message);
